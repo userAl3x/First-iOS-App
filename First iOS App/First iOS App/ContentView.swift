53e77ac264d12b9e786d062a61c75f4eff4095dd
Alex Jiménez <121909423+userAl3x@users.noreply.github.com>
2025-11-19 22:27:46 +0100

    First commit

---
+++ First iOS App/First iOS App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  First iOS App
+//
+//  Created by Alex Jiménez Quiñonero on 19/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ First iOS App/First iOS App/First_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_iOS_AppApp.swift
+//  First iOS App
+//
+//  Created by Alex Jiménez Quiñonero on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct First_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
