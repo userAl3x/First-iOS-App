72f2bd2a8947250d29fb8398c01e02da6598c73a
Alex Jiménez <121909423+userAl3x@users.noreply.github.com>
2025-11-23 22:43:08 +0100

    Creamos las carpetas Model y View, fichero Game, y movemos el ContentView al directorio Views

---
+++ First iOS App/First iOS App/Models/Game.swift
@@ -0,0 +1,7 @@
+//
+//  Game.swift
+//  First iOS App
+//
+//  Created by Alex Jiménez Quiñonero on 23/11/25.
+//
+
---
+++ First iOS App/First iOS App/Views/LeaderboardView.swift
@@ -0,0 +1,7 @@
+//
+//  LeaderboardView.swift
+//  First iOS App
+//
+//  Created by Alex Jiménez Quiñonero on 23/11/25.
+//
+
